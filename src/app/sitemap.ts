7726b0efacfea5dc90a7e9d500995b60b18c5bf0
innovatias <david@atias.me>
2025-10-06 16:45:00 +0700

    Fix sitemap domain from innovaly.com to innovaly.services to match production site

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://innovaly.com'
+  const baseUrl = 'https://innovaly.services'
   
   return [
     {
